f2f3210a68ae2cce048d4a81b30d093d8ebe8d4b
Toan Nguyen <ntuantoan@gmail.com>
2018-03-06 15:35:23 +0700

    Implement microposts with images

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,4 +3,14 @@ class Micropost < ApplicationRecord
   validates :content, presence: true, length: {maximum: 140}
   validates_presence_of :user_id
   default_scope ->{order created_at: :desc}
+  mount_uploader :picture, PictureUploader
+  validate :picture_size
+
+  private
+
+  # Validate the size of an uploaded picture
+  def picture_size
+    return unless picture.size > 5.megabytes
+    errors.add(:picture, "should be less than 5MB")
+  end
 end
